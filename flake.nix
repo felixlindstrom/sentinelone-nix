19c143931d8bd6863c4a81e559b3ab993ae850da
Ben Brown <ben@demerara.io>
2025-09-12 19:23:36 +0100

    chore: reduce flake dependencies
    This will thus reduce the number of flake inputs pinned for downstream
    consumers.

--- flake.nix
+++ flake.nix
@@ -3,8 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
